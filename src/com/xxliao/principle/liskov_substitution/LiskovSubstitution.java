d4e16a71d8e0c53e735cf4b6610986c210b31dc7
xxliao100 <xxliao100@163.com>
2024-05-22 16:13:28 +0800

    设计模式7大原则提交

---
+++ src/com/xxliao/principle/liskov_substitution/LiskovSubstitution.java
@@ -0,0 +1,17 @@
+package com.xxliao.principle.liskov_substitution;
+
+/**
+ * @author xxliao
+ * @description: 里氏替换原则
+ *
+ * 理解：
+ * 1.因为继承会给程序带来侵入性，程序的可移植性低。
+ *
+ * 2.里氏替换的核心思想是：所有引用基类的地方都必须透明地使用其子类对象，也就是在使用继承时候，在子类中尽量不要重写父类的方法。
+ * 3.继承实际上会让两个类的耦合性增加，在适当的情况下，可以通过聚合、组合、依赖来解决。
+ *
+ * @date 2024/5/21 23:40
+ */
+
+public class LiskovSubstitution {
+}
---
+++ src/com/xxliao/principle/single_responsibility/SingleResponsibility.java
@@ -0,0 +1,22 @@
+package com.xxliao.principle.single_responsibility;
+
+/**
+ *
+ * @description: 单一原则
+ *
+ * 单一原则：对于类来说，单一原则就是一个类只负责一项职责。
+ *
+ * 注意事项：
+ * 1.降低类的耦合度，一个类只负责一项职责。
+ * 2.提高类的可读性以及可维护性。
+ * 3.降低代码变更引起的风险。
+ * 4.通常情况下我们应当遵守单一原则，只有再逻辑足够简单的情况下才可以违反单一职责，只有类中的方法足够少，可以在方法上保持单一原则。
+ *
+ * @author xxliao
+ * @date 2024/5/21$ 22:16$
+ */
+
+public class SingleResponsibility {
+
+
+}
